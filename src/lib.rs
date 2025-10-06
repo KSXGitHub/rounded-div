5b2eab2ea73c7cd4544ff7098c7501d939bb8b60
Khải <hvksmr1996@gmail.com>
2025-10-06 08:05:45 +0700

    fix: prevent overflow in some edge cases (#32)
    * fix: prevent overflow in some edge cases
    
    Fixes https://github.com/KSXGitHub/rounded-div/issues/31
    
    * style: fix formatting
    
    * test: use `decimal-rs` for more accurate rounding
    
    * test: other signed integer types

--- src/lib.rs
+++ src/lib.rs
@@ -64,14 +64,50 @@ macro_rules! unsigned_div_fn {
 macro_rules! signed_div_fn {
     ($name:ident -> $data:ident do as $unsigned:ident) => {
         /// Get rounded result of an integer division.
-        #[inline]
         pub const fn $name(dividend: $data, divisor: $data) -> $data {
+            // Q: No negative sign before `$data::MIN` to invert it. Is it correct?
+            // A: It is correct and defined.
+            // Q: Why do it? Why don't we just use `-`?
+            // A: Because `-$data::MIN` would panic with overflow in debug mode.
+            const NEG_OF_MIN: $unsigned = $data::MIN as $unsigned;
+
+            // The following constants are necessary because older version of Rust does not
+            // support constant expressions in match arms.
+            const HALF_MIN: $data = $data::MIN / 2;
+            const HALF_MIN_PLUS_1: $data = HALF_MIN + 1;
+            const NEG_HALF_MIN: $data = -HALF_MIN;
+            const NEG_HALF_MIN_MINUS_1: $data = NEG_HALF_MIN - 1;
+            const HALF_MAX: $data = $data::MAX / 2;
+            const HALF_MAX_PLUS_1: $data = HALF_MAX + 1;
+            const NEG_HALF_MAX: $data = -HALF_MAX;
+            const NEG_HALF_MAX_MINUS_1: $data = NEG_HALF_MAX - 1;
+
             match (dividend, divisor) {
                 ($data::MIN, -1) => panic!("attempt to divide with overflow"),
-                (0.., 0..) => self::$unsigned(dividend as $unsigned, divisor as $unsigned) as $data,
-                (..0, 0..) => -(self::$unsigned(-dividend as $unsigned, divisor as $unsigned) as $data),
-                (0.., ..0) => -(self::$unsigned(dividend as $unsigned, -divisor as $unsigned) as $data),
-                (..0, ..0) => self::$unsigned(-dividend as $unsigned, -divisor as $unsigned) as $data,
+                ($data::MIN, $data::MIN) | ($data::MAX, $data::MAX) => 1,
+                ($data::MIN, $data::MAX) | ($data::MAX, $data::MIN) => -1,
+
+                (0, ..=-1 | 1..) => 0,
+                (..=-1 | 1.., 1) => dividend,
+                (..=-1 | 1.., -1) => -dividend,
+
+                (_, 0) => panic!("attempt to divide by zero"),
+
+                ($data::MIN, 1..) => -(self::$unsigned(NEG_OF_MIN, divisor as $unsigned) as $data),
+                ($data::MIN, ..=-1) => self::$unsigned(NEG_OF_MIN, -divisor as $unsigned) as $data,
+
+                (HALF_MIN_PLUS_1..=NEG_HALF_MIN_MINUS_1, $data::MIN) => 0, // MIN is even, so x/MIN where x.abs() < MIN/2 should be less than 0.5
+                (..=HALF_MIN, $data::MIN) => 1, // MIN is even so x/MIN where x <= MIN/2 should be greater than 0.5
+                (NEG_HALF_MIN.., $data::MIN) => -1, // MIN is even so x/MIN where x >= -MIN/2 should be less than -0.5
+
+                (NEG_HALF_MAX..=HALF_MAX, $data::MAX) => 0, // MAX is odd, so x/MAX where x.abs() <= MAX/2 should be less than 0.5
+                (HALF_MAX_PLUS_1.., $data::MAX) => 1, // MAX is odd so x/MAX where x > MAX/2 should be greater than 0.5
+                (..=NEG_HALF_MAX_MINUS_1, $data::MAX) => -1, // MAX is odd so x/MAX where x < -MAX/2 should be less than -0.5
+
+                (1.., 1..) => self::$unsigned(dividend as $unsigned, divisor as $unsigned) as $data,
+                (..=-1, 1..) => -(self::$unsigned(-dividend as $unsigned, divisor as $unsigned) as $data),
+                (1.., ..=-1) => -(self::$unsigned(dividend as $unsigned, -divisor as $unsigned) as $data),
+                (..=-1, ..=-1) => self::$unsigned(-dividend as $unsigned, -divisor as $unsigned) as $data,
             }
         }
 
---
+++ tests/signed_special_cases.rs
@@ -0,0 +1,236 @@
+/// Run test cases for a signed integer type.
+///
+/// **NOTE:** Due to the limitation of [`decimal_rs`], this macro cannot run on `i128`.
+macro_rules! cases {
+    ($name:ident) => {
+        #[cfg(test)]
+        mod $name {
+            use decimal_rs::Decimal;
+
+            fn slow(dividend: $name, divisor: $name) -> $name {
+                let dividend: Decimal = dividend.into();
+                let divisor: Decimal = divisor.into();
+                let (int_val, scale, negative) = (dividend / divisor).round(0).into_parts();
+                assert_eq!(scale, 0);
+                let int_val = int_val as i128;
+                let factor: i128 = match negative {
+                    true => -1,
+                    false => 1,
+                };
+                (int_val * factor) as $name
+            }
+
+            #[test]
+            fn min_max_combinations() {
+                assert_eq!(rounded_div::$name($name::MIN, $name::MIN), 1);
+                assert_eq!(rounded_div::$name($name::MAX, $name::MAX), 1);
+                assert_eq!(rounded_div::$name($name::MIN, $name::MAX), -1);
+                assert_eq!(rounded_div::$name($name::MAX, $name::MIN), -1);
+            }
+
+            #[test]
+            fn div_by_1() {
+                assert_eq!(rounded_div::$name($name::MIN, 1), $name::MIN);
+                assert_eq!(rounded_div::$name(-8, 1), -8);
+                assert_eq!(rounded_div::$name(-2, 1), -2);
+                assert_eq!(rounded_div::$name(-1, 1), -1);
+                assert_eq!(rounded_div::$name($name::MAX, 1), $name::MAX);
+                assert_eq!(rounded_div::$name(8, 1), 8);
+                assert_eq!(rounded_div::$name(2, 1), 2);
+                assert_eq!(rounded_div::$name(1, 1), 1);
+            }
+
+            #[test]
+            fn div_by_neg_1() {
+                assert_eq!(rounded_div::$name(-8, -1), 8);
+                assert_eq!(rounded_div::$name(-2, -1), 2);
+                assert_eq!(rounded_div::$name(-1, -1), 1);
+                assert_eq!(rounded_div::$name($name::MAX, -1), -$name::MAX);
+                assert_eq!(rounded_div::$name(8, -1), -8);
+                assert_eq!(rounded_div::$name(2, -1), -2);
+                assert_eq!(rounded_div::$name(1, -1), -1);
+            }
+
+            #[test]
+            fn div_small_by_min_max() {
+                assert_eq!(rounded_div::$name(0, $name::MIN), 0);
+                assert_eq!(rounded_div::$name(1, $name::MIN), 0);
+                assert_eq!(rounded_div::$name(2, $name::MIN), 0);
+                assert_eq!(rounded_div::$name(3, $name::MIN), 0);
+                assert_eq!(rounded_div::$name(4, $name::MIN), 0);
+                assert_eq!(rounded_div::$name(5, $name::MIN), 0);
+                assert_eq!(rounded_div::$name(-1, $name::MIN), 0);
+                assert_eq!(rounded_div::$name(-2, $name::MIN), 0);
+                assert_eq!(rounded_div::$name(-3, $name::MIN), 0);
+                assert_eq!(rounded_div::$name(-4, $name::MIN), 0);
+                assert_eq!(rounded_div::$name(-5, $name::MIN), 0);
+                assert_eq!(rounded_div::$name(0, $name::MAX), 0);
+                assert_eq!(rounded_div::$name(1, $name::MAX), 0);
+                assert_eq!(rounded_div::$name(2, $name::MAX), 0);
+                assert_eq!(rounded_div::$name(3, $name::MAX), 0);
+                assert_eq!(rounded_div::$name(4, $name::MAX), 0);
+                assert_eq!(rounded_div::$name(5, $name::MAX), 0);
+                assert_eq!(rounded_div::$name(-1, $name::MAX), 0);
+                assert_eq!(rounded_div::$name(-2, $name::MAX), 0);
+                assert_eq!(rounded_div::$name(-3, $name::MAX), 0);
+                assert_eq!(rounded_div::$name(-4, $name::MAX), 0);
+                assert_eq!(rounded_div::$name(-5, $name::MAX), 0);
+
+                // MIN is even, so x/MIN where x.abs() < MIN/2 should be less than 0.5 which should round to 0
+                assert_eq!(rounded_div::$name($name::MIN / 2 + 1, $name::MIN), 0);
+                assert_eq!(rounded_div::$name($name::MIN / 2 + 2, $name::MIN), 0);
+
+                // MAX is odd, so x/MAX where x.abs() <= MAX/2 should be less than 0.5 which should round to 0
+                assert_eq!(rounded_div::$name($name::MAX / 2, $name::MAX), 0);
+                assert_eq!(rounded_div::$name($name::MAX / 2 - 1, $name::MAX), 0);
+                assert_eq!(rounded_div::$name($name::MAX / 2 - 2, $name::MAX), 0);
+            }
+
+            #[test]
+            fn div_big_by_min_max() {
+                assert_eq!($name::MIN & 1, 0); // assert that MIN is even
+                assert_eq!($name::MAX & 1, 1); // assert that MAX is odd
+
+                // MIN is even so x/MIN where x <= MIN/2 should be greater than 0.5 which should round to 1
+                assert_eq!(rounded_div::$name($name::MIN / 2, $name::MIN), 1);
+                assert_eq!(rounded_div::$name($name::MIN / 2 - 1, $name::MIN), 1);
+                assert_eq!(rounded_div::$name($name::MIN / 2 - 2, $name::MIN), 1);
+                assert_eq!(rounded_div::$name($name::MIN, $name::MIN), 1);
+                assert_eq!(rounded_div::$name($name::MIN + 1, $name::MIN), 1);
+                assert_eq!(rounded_div::$name($name::MIN + 2, $name::MIN), 1);
+
+                // MIN is even so x/MIN where x >= -MIN/2 should be less than -0.5 which should round to -1
+                assert_eq!(rounded_div::$name(-($name::MIN / 2), $name::MIN), -1);
+                assert_eq!(rounded_div::$name(-($name::MIN / 2) + 1, $name::MIN), -1);
+                assert_eq!(rounded_div::$name(-($name::MIN / 2) + 2, $name::MIN), -1);
+                assert_eq!(rounded_div::$name($name::MAX, $name::MIN), -1);
+                assert_eq!(rounded_div::$name($name::MAX - 1, $name::MIN), -1);
+                assert_eq!(rounded_div::$name($name::MAX - 2, $name::MIN), -1);
+
+                // MAX is odd so x/MAX where x > MAX/2 should be greater than 0.5 which should round to 1
+                assert_eq!(rounded_div::$name($name::MAX / 2 + 1, $name::MAX), 1);
+                assert_eq!(rounded_div::$name($name::MAX / 2 + 2, $name::MAX), 1);
+                assert_eq!(rounded_div::$name($name::MAX, $name::MAX), 1);
+                assert_eq!(rounded_div::$name($name::MAX - 1, $name::MAX), 1);
+                assert_eq!(rounded_div::$name($name::MAX - 2, $name::MAX), 1);
+
+                // MAX is odd so x/MAX where x < -MAX/2 should be less than -0.5 which should round to -1
+                assert_eq!(rounded_div::$name(-($name::MAX / 2) - 1, $name::MAX), -1);
+                assert_eq!(rounded_div::$name(-($name::MAX / 2) - 2, $name::MAX), -1);
+                assert_eq!(rounded_div::$name($name::MIN, $name::MAX), -1);
+                assert_eq!(rounded_div::$name($name::MIN + 1, $name::MAX), -1);
+                assert_eq!(rounded_div::$name($name::MIN + 2, $name::MAX), -1);
+            }
+
+            #[test]
+            fn div_min_max_by_small() {
+                assert_eq!(rounded_div::$name($name::MIN, 1), $name::MIN);
+                assert_eq!(rounded_div::$name($name::MIN, 2), slow($name::MIN, 2));
+                assert_eq!(rounded_div::$name($name::MIN, 3), slow($name::MIN, 3));
+                assert_eq!(rounded_div::$name($name::MIN, 4), slow($name::MIN, 4));
+                assert_eq!(rounded_div::$name($name::MIN, 5), slow($name::MIN, 5));
+                assert_eq!(rounded_div::$name($name::MIN, 6), slow($name::MIN, 6));
+                assert_eq!(rounded_div::$name($name::MIN, 7), slow($name::MIN, 7));
+                assert_eq!(rounded_div::$name($name::MAX, 1), $name::MAX);
+                assert_eq!(rounded_div::$name($name::MAX, 2), slow($name::MAX, 2));
+                assert_eq!(rounded_div::$name($name::MAX, 3), slow($name::MAX, 3));
+                assert_eq!(rounded_div::$name($name::MAX, 4), slow($name::MAX, 4));
+                assert_eq!(rounded_div::$name($name::MAX, 5), slow($name::MAX, 5));
+                assert_eq!(rounded_div::$name($name::MAX, 6), slow($name::MAX, 6));
+                assert_eq!(rounded_div::$name($name::MAX, 7), slow($name::MAX, 7));
+            }
+
+            #[test]
+            fn div_min_max_by_big() {
+                assert_eq!(rounded_div::$name($name::MIN, $name::MIN / 2), 2);
+                assert_eq!(rounded_div::$name($name::MIN, $name::MIN / 2 - 1), 2);
+                assert_eq!(rounded_div::$name($name::MIN, $name::MIN / 2 - 2), 2);
+                assert_eq!(rounded_div::$name($name::MIN, $name::MIN), 1);
+                assert_eq!(rounded_div::$name($name::MIN, $name::MIN + 1), 1);
+                assert_eq!(rounded_div::$name($name::MIN, $name::MIN + 2), 1);
+
+                assert_eq!(rounded_div::$name($name::MIN, -($name::MIN / 2)), -2);
+                assert_eq!(rounded_div::$name($name::MIN, -($name::MIN / 2) + 1), -2);
+                assert_eq!(rounded_div::$name($name::MIN, -($name::MIN / 2) + 2), -2);
+                assert_eq!(rounded_div::$name($name::MIN, $name::MAX), -1);
+                assert_eq!(rounded_div::$name($name::MIN, $name::MAX - 1), -1);
+                assert_eq!(rounded_div::$name($name::MIN, $name::MAX - 2), -1);
+
+                assert_eq!(rounded_div::$name($name::MAX, $name::MAX / 2), 2);
+                assert_eq!(rounded_div::$name($name::MAX, $name::MAX / 2 + 1), 2);
+                assert_eq!(rounded_div::$name($name::MAX, $name::MAX / 2 + 2), 2);
+                assert_eq!(rounded_div::$name($name::MAX, $name::MAX), 1);
+                assert_eq!(rounded_div::$name($name::MAX, $name::MAX - 1), 1);
+                assert_eq!(rounded_div::$name($name::MAX, $name::MAX - 2), 1);
+
+                assert_eq!(rounded_div::$name($name::MAX, -($name::MAX / 2)), -2);
+                assert_eq!(rounded_div::$name($name::MAX, -($name::MAX / 2) - 1), -2);
+                assert_eq!(rounded_div::$name($name::MAX, -($name::MAX / 2) - 2), -2);
+                assert_eq!(rounded_div::$name($name::MAX, $name::MIN), -1);
+                assert_eq!(rounded_div::$name($name::MAX, $name::MIN + 1), -1);
+                assert_eq!(rounded_div::$name($name::MAX, $name::MIN + 2), -1);
+            }
+
+            #[test]
+            fn equal_absolutes() {
+                assert_eq!(rounded_div::$name(1, 1), 1);
+                assert_eq!(rounded_div::$name(2, 2), 1);
+                assert_eq!(rounded_div::$name(3, 3), 1);
+                assert_eq!(rounded_div::$name(4, 4), 1);
+                assert_eq!(rounded_div::$name(5, 5), 1);
+                assert_eq!(rounded_div::$name(1, -1), -1);
+                assert_eq!(rounded_div::$name(2, -2), -1);
+                assert_eq!(rounded_div::$name(3, -3), -1);
+                assert_eq!(rounded_div::$name(4, -4), -1);
+                assert_eq!(rounded_div::$name(5, -5), -1);
+                assert_eq!(rounded_div::$name(-1, 1), -1);
+                assert_eq!(rounded_div::$name(-2, 2), -1);
+                assert_eq!(rounded_div::$name(-3, 3), -1);
+                assert_eq!(rounded_div::$name(-4, 4), -1);
+                assert_eq!(rounded_div::$name(-5, 5), -1);
+                assert_eq!(rounded_div::$name(-1, -1), 1);
+                assert_eq!(rounded_div::$name(-2, -2), 1);
+                assert_eq!(rounded_div::$name(-3, -3), 1);
+                assert_eq!(rounded_div::$name(-4, -4), 1);
+                assert_eq!(rounded_div::$name(-5, -5), 1);
+                assert_eq!(rounded_div::$name($name::MAX, $name::MAX), 1);
+                assert_eq!(rounded_div::$name(-$name::MAX, $name::MAX), -1);
+                assert_eq!(rounded_div::$name($name::MAX, -$name::MAX), -1);
+                assert_eq!(rounded_div::$name(-$name::MAX, -$name::MAX), 1);
+            }
+
+            #[cfg(test)]
+            mod div_by_0 {
+                #[test]
+                #[should_panic(expected = "attempt to divide by zero")]
+                fn div_0_by_0() {
+                    let _ = rounded_div::$name(0, 0);
+                }
+
+                #[test]
+                #[should_panic(expected = "attempt to divide by zero")]
+                fn div_min_by_0() {
+                    let _ = rounded_div::$name($name::MIN, 0);
+                }
+
+                #[test]
+                #[should_panic(expected = "attempt to divide by zero")]
+                fn div_max_by_0() {
+                    let _ = rounded_div::$name($name::MAX, 0);
+                }
+
+                #[test]
+                #[should_panic(expected = "attempt to divide by zero")]
+                fn div_1_by_0() {
+                    let _ = rounded_div::$name(1, 0);
+                }
+            }
+        }
+    };
+}
+
+cases!(i8);
+cases!(i16);
+cases!(i32);
+cases!(i64);
+cases!(isize);
